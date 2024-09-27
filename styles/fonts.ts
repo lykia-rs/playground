f6e851ba4fbafe19d2a67d137ff92de4c2efb492
Vedat Can Keklik <vcankeklik@gmail.com>
2024-09-27 20:15:33 +0300

    init: Moved to new repo

---
+++ styles/fonts.ts
@@ -0,0 +1,6 @@
+import { JetBrains_Mono, Inter } from 'next/font/google'
+
+const defaultFont = Inter({ weight: '400', subsets: ['latin'] })
+const jetBrainsMono = JetBrains_Mono({ weight: '400', subsets: ['latin'] })
+
+export { defaultFont, jetBrainsMono }
